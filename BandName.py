9b88dc7beed7c403d8028310b9a0bb6ddadc3d98
Lungile-Tshatshu <LungileLolwane025@gmail.com>
2024-10-04 09:04:24 +0200

    Day 1

---
+++ BandName.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!")
+name_of_your_city = input("Enter the name of your city\n")
+name_0f_your_pet = input("Enter the name of your pet\n")
+print("The name of you band could be: "+name_of_your_city+" "+name_0f_your_pet)
