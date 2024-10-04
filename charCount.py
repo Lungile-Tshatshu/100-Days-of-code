c22b3dff85936abeba5d581d403d13220e37c854
Lungile-Tshatshu <LungileLolwane025@gmail.com>
2024-10-04 22:57:16 +0200

    Add files via upload
    Day 2

---
+++ charCount.py
@@ -0,0 +1,7 @@
+UserName = input("Enter your name:\n")
+UserSurname = input("Enter your surname:\n")
+
+Name_Length = len(UserName)
+Surname_Length = len(UserSurname)
+
+print("The number of characters in your name and surname is"+" "+str(Name_Length)+" "+"and "+str(Surname_Length)+" "+"respectively")
